fb00ed36fe7df35236634b21d27669222054c871
valentinaq13 <emeritavalentina@gmail.com>
2022-07-31 14:37:57 -0300

    show/hide button

--- Angular/homewoks/src/app/app.component.ts
+++ Angular/homewoks/src/app/app.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'homewoks';
+  list = true;
+  productosDelSuper = [
+    { nombre: "Queso", precio: 10},
+    { nombre: "Gaseosa", precio: 5},
+    { nombre: "Cerveza", precio: 20},
+    { nombre: "Arroz", precio: 7},
+    { nombre: "Fideos", precio: 5},
+    { nombre: "Lavandina", precio: 9},
+    { nombre: "Shampoo", precio: 3},
+    { nombre: "Jabon", precio: 4},
+  ];
+
+  hideList() {
+    this.list = false;
+  };
+  showList(){
+    this.list = true
+  }
 }
 
